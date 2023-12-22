d4ea6f506616844b5922337706545a4301d9d9e9
Luis-Castelblanco <88451449+Luis-Castelblanco@users.noreply.github.com>
2023-12-22 11:41:30 -0600

    Create SoilGrids_download.r

---
+++ SoilGrids_download.r
@@ -0,0 +1,5 @@
+gdalwarp(t_srs="EPSG:4326", multi=TRUE, wm=200, 
+         co=c("BIGTIFF=YES", "COMPRESS=DEFLATE", "TILED=TRUE"),
+         tr=c(0.25,0.25), # Desired output resolution
+         "/vsicurl?max_retry=3&retry_delay=1&list_dir=no&url=https://files.isric.org/soilgrids/latest/data/wrb/MostProbable.vrt", # Input VRT
+         "MostProbable_SoilGrids2.tif") # Output file
